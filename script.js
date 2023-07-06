20ca10702ce01ee341645614755ee52e520f2346
Sthabiraj <birajshrestha2007@gmail.com>
2023-07-06 20:59:43 +0545

    Updated error validation

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ const checkPersonalInfo = () => {
     document.querySelector("#email").value,
     document.querySelector("#phone").value,
   ];
-  const validInfo = [];
   // Condition for null input
   for (let i = 0; i < personalInfo.length; i++) {
     if (personalInfo[i] == "") {
@@ -33,39 +32,23 @@ const checkPersonalInfo = () => {
     }
   }
   // Name validation
-  if (personalInfo[0] != "") {
-    let nameRegex = /^[A-Za-z\s'-]+$/;
-    if (!personalInfo[0].match(nameRegex)) {
-      errorLabel[0].innerText = "Name should be valid";
-      inputBox[0].classList.add("error");
-    } else {
-      errorLabel[0].innerText = "";
-      inputBox[0].classList.remove("error");
-      validInfo[0] = personalInfo[0];
-    }
-  }
-  // Email validation
-  if (personalInfo[1] != "") {
-    let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+/;
-    if (!personalInfo[1].match(emailRegex)) {
-      errorLabel[1].innerText = "Email should be valid";
-      inputBox[1].classList.add("error");
-    } else {
-      errorLabel[1].innerText = "";
-      inputBox[1].classList.remove("error");
-      validInfo[1] = personalInfo[1];
-    }
-  }
-  // Phone number validation
-  if (personalInfo[2] != "") {
-    let phoneRegex = /^\d{10}$/;
-    if (!personalInfo[2].match(phoneRegex)) {
-      errorLabel[2].innerText = "Phone number should be valid";
-      inputBox[2].classList.add("error");
-    } else {
-      errorLabel[2].innerText = "";
-      inputBox[2].classList.remove("error");
-      validInfo[2] = personalInfo[2];
+  const regex = [/^[A-Za-z\s'-]+$/, /^[^\s@]+@[^\s@]+\.[^\s@]+/, /^\d{10}$/];
+  const errorMsg = [
+    "Name should be valid",
+    "Email should be valid",
+    "Phone should be valid",
+  ];
+  const validInfo = [];
+  for (let i = 0; i < personalInfo.length; i++) {
+    if (personalInfo[i] != "") {
+      if (!personalInfo[i].match(regex[i])) {
+        errorLabel[i].innerText = errorMsg[i];
+        inputBox[i].classList.add("error");
+      } else {
+        errorLabel[i].innerText = "";
+        inputBox[i].classList.remove("error");
+        validInfo[i] = personalInfo[i];
+      }
     }
   }
   return validInfo;
